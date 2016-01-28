bc3d8986c389f8c122107a737c667e0f9a09e505
Adam Li <adamskino@gmail.com>
2016-01-28 02:33:54 -0800

    Finish create.sql and drop.sql; In create.sql varchar length may need to change according to data later

---
+++ create.sql
@@ -0,0 +1,65 @@
+CREATE TABLE ItemTable (
+	ItemID			INT	NOT NULL,
+	Name			VARCHAR(100) NOT NULL,
+	Currently 		INT NOT NULL,
+	First_Bid		INT	NOT NULL,
+	Number_of_Bids	INT	NOT NULL,
+	Location		VARCHAR(40) NOT NULL,
+	Country			VARCHAR(40) NOT NULL,
+	Started			TIMESTAMP NOT NULL,
+	Ends			TIMESTAMP NOT NULL,
+	SellerID		INT	NOT NULL,
+	Description		VARCHAR(100) NOT NULL,
+	PRIMARY KEY (ItemID)
+);
+
+CREATE TABLE ItemCategory (
+	ItemID			INT	NOT NULL,
+	Category		VARCHAR(100) NOT NULL,
+	PRIMARY KEY (ItemID)
+);
+
+CREATE TABLE ItemBuyPrice (
+	ItemID			INT	NOT NULL,
+	BuyPrice		INT NOT NULL,
+	PRIMARY KEY (ItemID)
+);
+
+CREATE TABLE ItemBids (
+	ItemID			INT	NOT NULL,
+	BidderID		INT	NOT NULL,
+	Time 			TIMESTAMP NOT NULL,
+	Amount			INT NOT NULL,
+	PRIMARY KEY (ItemID, BidderID, Time)
+);
+
+CREATE TABLE ItemPosition (
+	ItemID			INT	NOT NULL,
+	Latitude		REAL, -- could be NULL when longitude is provided
+	Longitude		REAL, -- could be NULL when latitude is provided
+	PRIMARY KEY (ItemID)
+);
+
+CREATE TABLE SellerRating (
+	UserID			INT	NOT NULL,
+	Rating			INT	NOT NULL,
+	PRIMARY KEY (UserID)
+);
+
+CREATE TABLE BidderRating (
+	UserID			INT	NOT NULL,
+	Rating			INT	NOT NULL,
+	PRIMARY KEY (UserID)
+);
+
+CREATE TABLE UserLocation (
+	UserID			INT	NOT NULL,
+	Location		VARCHAR(40) NOT NULL,
+	PRIMARY KEY (UserID)
+);
+
+CREATE TABLE UserCountry (
+	UserID			INT	NOT NULL,
+	Country			VARCHAR(40) NOT NULL,
+	PRIMARY KEY (UserID)
+);
---
+++ drop.sql
@@ -0,0 +1,10 @@
+DROP TABLE ItemTable;
+DROP TABLE ItemCategory;
+DROP TABLE ItemBuyPrice;
+DROP TABLE ItemBids;
+DROP TABLE ItemPosition;
+DROP TABLE SellerRating;
+DROP TABLE BidderRating;
+DROP TABLE UserLocation;
+DROP TABLE UserCountry;
+
